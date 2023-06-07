830b5b5140112dd81a20de4f6fdd8f7798d64440
Mamun-Ur-Rashid <engmamun656@gmail.com>
2023-06-07 12:27:29 +0600

    added admin pannel

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
-const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY)
+const jwt = require('jsonwebtoken');
+
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-
+console.log(process.env.PAYMENT_SECRET_KEY);
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -47,6 +49,7 @@ async function run() {
     const menuCollection = client.db("bristodb").collection('menu');
     const reViewsCollection = client.db("bristodb").collection('revies');
     const cartCollection = client.db("bristodb").collection('carts');
+    const paymentCollection = client.db("bristodb").collection('payments');
     // jwt
     app.post('/jwt', (req, res) => {
       const user = req.body;
@@ -67,17 +70,17 @@ async function run() {
     }
 
     // user collection api
-    app.get('/users', verifyJWT,verifyAdmin, async(req, res) => {
+    app.get('/users', verifyJWT,verifyAdmin, async( req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
     })
 
     app.post('/users', async(req, res) =>{
       const user = req.body;
-      console.log(user);
+      // console.log(user);
       const query = {email : user.email};
       const existingUser = await userCollection.findOne(query);
-      console.log("esixting user",existingUser);
+      // console.log("esixting user",existingUser);
       if(existingUser){
         return res.send({message: 'user Already exists'});
       }
@@ -88,21 +91,21 @@ async function run() {
     app.get('/users/admin/:email', verifyJWT, async(req, res) => {
       const email = req.params.email;
       if(req.decoded.email !== email){
-        res.send({admin: false});
+        return res.send({admin: false});
       }
       const query = { email: email};
       const user = await userCollection.findOne(query);
       const result = {admin: user?.role === 'admin'};
       res.send(result);
     })
-    app.patch('/users/admin/:id', async(req, res) => {
+    app.patch('/users/admin/:id', async (req, res) => {
       const id = req.params.id;
       const filter = { _id : new ObjectId(id)};
       const updateDoc = {
         $set : {
           role: 'admin'
-        }
-      }
+        },
+      };
       const result = await userCollection.updateOne(filter, updateDoc);
       res.send(result);
     })
@@ -115,6 +118,12 @@ async function run() {
     app.post('/menu',verifyJWT, verifyAdmin, async(req, res)=> {
       const newItem = req.body;
       const result = await menuCollection.insertOne(newItem);
+      res.send(result);
+    })
+    app.delete('/menu/:id', verifyJWT, verifyAdmin, async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await menuCollection.deleteOne(query);
       res.send(result);
     })
     app.get('/revies', async(req, res) => {
@@ -130,7 +139,7 @@ async function run() {
       }
       const decodedEmail = req.decoded.email;
       if(email !== decodedEmail){
-        return res.status(403).send({ error: true, message: 'providen access'})
+        return res.status(403).send({ error: true, message: 'forbidden access'})
       }
       const query = { email: email};
       const result = await cartCollection.find(query).toArray();
@@ -150,9 +159,96 @@ async function run() {
       const result = await cartCollection.deleteOne(query);
       res.send(result);
     })
+    // // payment method
+    // app.post('/create-payment-intent', async (req, res) => {
+    //   const {price} = req.body;
+    //   const amount = parseInt(price * 100);
+    //   console.log(amount);
+    //   const paymentIntent = await stripe.paymentIntents.create({
+    //     amount : amount,
+    //     currency : 'usd',
+    //     payment_method_types: ['card']
+    //   });
+    //   res.send({
+    //     clientSecret : paymentIntent.client_secret
+    //   })
+    // })
+    // create payment intent
+    app.post("/create-payment-intent", verifyJWT, async (req, res) => {
+      const { price } = req.body;
+      // console.log(price);
+      const amount = parseInt(price * 100);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+    // payment related api
+    app.post('/payments',verifyJWT, async (req, res) => {
+      const payment = req.body;
+      const insertResult = await paymentCollection.insertOne(payment);
+
+      const query = {_id: {$in: payment.cartItems.map(id => new ObjectId(id))}};
+      const deleteResult = await cartCollection.deleteMany(query);
+
+      res.send({insertResult, deleteResult});
+    })
+    app.get('/admin-stats',verifyJWT, verifyAdmin, async (req, res) => {
+      const users = await userCollection.estimatedDocumentCount();
+      const products = await menuCollection.estimatedDocumentCount();
+      const orders = await paymentCollection.estimatedDocumentCount();
+      // total price of payment collection
+      const payments = await paymentCollection.find().toArray();
+      const revenue = payments.reduce((sum, payment) => sum + payment.price, 0)
+      res.send({
+        users, products, orders, revenue
+      })
+    })
+    app.get('/order-stats', verifyJWT, verifyAdmin, async(req, res) =>{
+      const pipeline = [
+        {
+          $lookup: {
+            from: 'menu',
+            localField: 'menuItems',
+            foreignField: '_id',
+            as: 'menuItemsData'
+          }
+        },
+        {
+          $unwind: '$menuItemsData'
+        },
+        {
+          $group: {
+            _id: '$menuItemsData.category',
+            count: { $sum: 1 },
+            total: { $sum: '$menuItemsData.price' }
+          }
+        },
+        {
+          $project: {
+            category: '$_id',
+            count: 1,
+            total: { $round: ['$total', 2] },
+            _id: 0
+          }
+        }
+      ];
+
+      const result = await paymentCollection.aggregate(pipeline).toArray()
+      res.send(result)
+
+    })
+
+
+
 
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
